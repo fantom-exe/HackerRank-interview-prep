5de452354c63b0344f507086ffbd6e4ce2b4bea3
Edward <fantom-exe@tuta.io>
2022-05-08 04:51:44 -0700

    finish mini-max

--- mini-max-sum.py
+++ mini-max-sum.py
@@ -13,10 +13,13 @@ import sys
 
 def miniMaxSum(arr):
     # Write your code here
-    min_sum = 0
-    max_sum = 0
-    for i in range(len(arr)):
+    arr.sort()
+    # first 4
+    min_sum = sum(arr[0:4])
+    # last 4
+    max_sum = sum(arr[1:5])
 
+    print(min_sum, max_sum)
 
 if __name__ == '__main__':
     arr = list(map(int, input().rstrip().split()))
