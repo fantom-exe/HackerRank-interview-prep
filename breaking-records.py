f534f1bea8cd29c7f5f657604538b1c2f15bf92b
Edward <fantom-exe@tuta.io>
2022-05-11 00:51:48 -0700

    write for loop & conditionals

--- breaking-records.py
+++ breaking-records.py
@@ -5,11 +5,22 @@ import re
 import sys
 
 
-def breakingRecords(scores):
-    # Write your code here
-    
+def breakingRecords(scores) -> int:
+    records = [[]]
+    min_rec = 0
+    max_rec = 0
+    for i in range(len(scores)):
+        score = scores[i]
+        if score > max_rec:
+            max_rec = score
+        elif score < min_rec:
+            min_rec = score
+
+        # records.append([min_rec][max_rec])
+    print(records)
+
+    return records
 
 
-
-scores = []
+scores = [12, 24, 10, 24]
 breakingRecords(scores)
