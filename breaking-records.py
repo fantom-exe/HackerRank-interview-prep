c13d9a5127b06a08324315244bdd22e3b5823191
Edward <fantom-exe@tuta.io>
2022-05-11 01:20:08 -0700

    finished

--- breaking-records.py
+++ breaking-records.py
@@ -1,26 +1,20 @@
-import math
-import os
-import random
-import re
-import sys
 
-
-def breakingRecords(scores) -> int:
-    records = [[]]
-    min_rec = 0
-    max_rec = 0
+def breakingRecords(scores) -> list[int]:
+    # init vals
+    min_rec = scores[0]
+    max_rec = scores[0]
+    records = [0, 0]  # Index 0 = breaking most points records | index 1 = breaking least points records
     for i in range(len(scores)):
         score = scores[i]
         if score > max_rec:
             max_rec = score
-        elif score < min_rec:
+            records[0] += 1
+        if score < min_rec:
             min_rec = score
-
-        # records.append([min_rec][max_rec])
-    print(records)
+            records[1] += 1
 
     return records
 
 
-scores = [12, 24, 10, 24]
+scores = [10, 5, 20, 20, 4, 5, 2, 25, 1]
 breakingRecords(scores)
