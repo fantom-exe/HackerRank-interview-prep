3e4fe418502f9c9293f8e6e49d6c4ae2b2de7c3c
Edward <fantom-exe@tuta.io>
2022-05-11 00:00:43 -0700

    cleanup

--- breaking-records.py
+++ breaking-records.py
@@ -7,20 +7,9 @@ import sys
 
 def breakingRecords(scores):
     # Write your code here
+    
 
 
 
-
-# if __name__ == '__main__':
-#     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-#
-#     n = int(input().strip())
-#
-#     scores = list(map(int, input().rstrip().split()))
-#
-#     result = breakingRecords(scores)
-#
-#     fptr.write(' '.join(map(str, result)))
-#     fptr.write('\n')
-#
-#     fptr.close()
+scores = []
+breakingRecords(scores)
