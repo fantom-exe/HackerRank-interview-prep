6adbd6b76ec40ee8651945634bedcf996bd1eaff
Edward <fantom-exe@tuta.io>
2022-05-10 23:58:45 -0700

    start breaking-records

--- breaking-records.py
+++ breaking-records.py
@@ -4,12 +4,6 @@ import random
 import re
 import sys
 
-#
-# Complete the 'breakingRecords' function below.
-#
-# The function is expected to return an INTEGER_ARRAY.
-# The function accepts INTEGER_ARRAY scores as parameter.
-#
 
 def breakingRecords(scores):
     # Write your code here
@@ -17,16 +11,16 @@ def breakingRecords(scores):
 
 
 
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    n = int(input().strip())
-
-    scores = list(map(int, input().rstrip().split()))
-
-    result = breakingRecords(scores)
-
-    fptr.write(' '.join(map(str, result)))
-    fptr.write('\n')
-
-    fptr.close()
+# if __name__ == '__main__':
+#     fptr = open(os.environ['OUTPUT_PATH'], 'w')
+#
+#     n = int(input().strip())
+#
+#     scores = list(map(int, input().rstrip().split()))
+#
+#     result = breakingRecords(scores)
+#
+#     fptr.write(' '.join(map(str, result)))
+#     fptr.write('\n')
+#
+#     fptr.close()
