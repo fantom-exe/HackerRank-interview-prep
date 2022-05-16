f11819f57821cdd9aa50375bf43caa079f700f0a
Edward <fantom-exe@tuta.io>
2022-05-16 15:29:29 -0700

    remove comment

--- divisible-sum-pairs.py
+++ divisible-sum-pairs.py
@@ -1,10 +1,4 @@
-#
-# The function is expected to return an INTEGER.
-# The function accepts following parameters:
-#  1. INTEGER n
-#  2. INTEGER k
-#  3. INTEGER_ARRAY ar
-#
+
 
 def divisibleSumPairs(n, k, ar):
 
