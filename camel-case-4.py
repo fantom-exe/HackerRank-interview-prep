5e5504812b4d85305bbb125a9f7ff3546bae242e
Edward <fantom-exe@tuta.io>
2022-05-16 21:15:37 -0700

    testing

--- camel-case-4.py
+++ camel-case-4.py
@@ -1,15 +1,35 @@
-
-def combine():
-
-
-def split():
-
-
-def camel_case(line):
-    str_list = line.split()
-
-    print(output)
+# def combiner(op, text):
+#     match op:
+#         case 'M':
+#
+#         case 'C':
+#
+#         case 'V':
 
 
-line = input()
-print(camel_case(line))
+def spliter(op, text):
+    match op:
+        case 'M':
+            print(text)
+        # case 'C':
+        #
+        # case 'V':
+
+
+
+def camel_case(text_in):
+    str_list = text_in.split(';')
+    op1 = str_list[0]
+    op2 = str_list[1]
+    text = str_list[2]
+
+    match op1:
+        case 'S':
+            spliter(op2, text)
+        case 'C':
+            # combiner(op2, text)
+
+
+
+text_in = 'S;M;plasticCup()'
+print(camel_case(text_in))
