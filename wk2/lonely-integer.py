4badb81bad79da60d1b1ce8ffd39dc22d1a10b6d
Edward <fantom-exe@tuta.io>
2022-08-30 08:43:18 -0700

    use counter

--- wk2/lonely-integer.py
+++ wk2/lonely-integer.py
@@ -2,18 +2,21 @@
 
 def lonelyinteger(a):
     size = len(a)
+
     for i in range(size):
-        if i == size:
+        count = 0
+
+        for j in range(size):
+            if a[i] == a[j]:
+                count += 1
+            if count == 2:
+                break
+
+        if count == 1:
             return a[i]
 
-        for j in range(i+1, size):
-            if a[i] == a[j]:
-                break
-            elif a[i] != a[j] and j == size:
-                return a[j]
 
-
-a = [1,2,3,4,3,2,1]
+a = [1,2,2,4,3,3,1]
 print(lonelyinteger(a))
 a = [1,1,3,7,2,3,2,4,7]
 print(lonelyinteger(a))
