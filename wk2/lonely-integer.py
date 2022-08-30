5332c14bd51f4114a5c9bd6a0cd2102779e49c2a
Edward <eisajanyan@tuta.io>
2022-08-29 19:58:41 -0700

    init

---
+++ wk2/lonely-integer.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def lonelyinteger(a):
+
+
+
+
+a = [1,2,3,4,3,2,1]
+lonelyinteger(a)
