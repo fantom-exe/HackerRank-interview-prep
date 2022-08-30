9c7edc4975a8c931de700fad5a60ac6d2db9a21c
Edward <eisajanyan@tuta.io>
2022-08-29 21:30:04 -0700

    diff approach

--- wk2/lonely-integer.py
+++ wk2/lonely-integer.py
@@ -1,26 +1,17 @@
 #!/bin/python3
 
-import math
-import os
-import random
-import re
-import sys
-
-
 def lonelyinteger(a):
     size = len(a)
-    i = 0
-    j = size-1
+    for i in range(size):
+        for j in range(i+1, size):
+            if a[i] == a[j]:
+                lonely =
 
-    while i != j:
-
-
-        # check if array over
-        if i == j:
-            lonely = a[i]
 
     return lonely
 
 
 a = [1,2,3,4,3,2,1]
+lonelyinteger(a)
+a = [1,1,3,7,2,3,2,4,7]
 lonelyinteger(a)
