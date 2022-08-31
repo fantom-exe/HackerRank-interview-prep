80d6d8f5e6bbd7f71ccfcf413383e3dbc3788627
Edward <fantom-exe@tuta.io>
2022-08-31 16:54:30 -0700

    testing

--- wk2/diagonal-difference.py
+++ wk2/diagonal-difference.py
@@ -2,8 +2,25 @@
 
 
 def diagonalDifference(arr):
+    primary_diag_sum = 0
+    secondary_diag_sum = 0
+
+    # primary diagonal
+    for row in range(len(arr)):
+        col = row
+        primary_diag_sum += arr[row][col]
+        print(primary_diag_sum)
+
+    # secondary diagonal
+    for row in reversed(range(len(arr))):
+        col = row
+        secondary_diag_sum += arr[row][col]
+        print(secondary_diag_sum)
+
+    return abs(primary_diag_sum - secondary_diag_sum)
 
 
-
-arr = [[11, 2, 4], [4, 5, 6], [10, 8, -12]]
+arr = [[11, 2, 4],
+       [4, 5, 6],
+       [10, 8, -12]]
 print(diagonalDifference(arr))
