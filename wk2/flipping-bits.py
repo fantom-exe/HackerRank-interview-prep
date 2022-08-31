13efebb5af332eddad1eb1f956ae14fb7e64220c
Edward <fantom-exe@tuta.io>
2022-08-31 01:25:35 -0700

    finished binary converter

--- wk2/flipping-bits.py
+++ wk2/flipping-bits.py
@@ -2,13 +2,24 @@
 
 
 def decimalToBinary(decimal):
+    binary = [0] * 32
+    index = 31
 
+    while True:
+        remainder = decimal % 2
+        decimal //= 2
+        binary[index] = remainder
+        index -= 1
+
+        print(decimal, remainder, binary)
+
+        if decimal == 0:
+            break
 
     return binary
 
 
 def binaryToDecimal(binary):
-
 
     return decimal
 
@@ -18,17 +29,20 @@ def flippingBits(n):
     binary = decimalToBinary(n)
 
     # flip bits
-
+    for i in binary:
+        break
 
     # convert back to decimal
-    dec_flipped = binaryToDecimal(binary)
+    decimal_flipped = binaryToDecimal(binary)
 
-    return dec_flipped
+    return decimal_flipped
 
 
-n = 2147483647
+n = 13
 print(flippingBits(n))
-n = 1
-print(flippingBits(n))
-n = 0
-print(flippingBits(n))
+# n = 2147483647
+# print(flippingBits(n))
+# n = 1
+# print(flippingBits(n))
+# n = 0
+# print(flippingBits(n))
