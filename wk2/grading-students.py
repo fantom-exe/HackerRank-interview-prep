0fd921eab2e6f28010ae0bebbec29cca20f6c89d
Edward <fantom-exe@tuta.io>
2022-08-30 20:50:37 -0700

    done

--- wk2/grading-students.py
+++ wk2/grading-students.py
@@ -2,10 +2,16 @@
 
 
 def gradingStudents(grades):
+    for i in range(len(grades)):
+        grade = grades[i]
+
+        if grade > 40:
+            nearest = grade % 5
+            if nearest >= 3:
+                grades[i] += (5 - nearest)
+
+    return grades
 
 
-
-
-
-grades = [84, 29, 57, 40]
+grades = [84, 29, 57, 40, 58, 11, 57, 59]
 print(gradingStudents(grades))
