3c512587a0f7c58d5a738a7b9bcba00aa79a881c
Edward <fantom-exe@tuta.io>
2022-09-01 23:58:55 -0700

    init

--- wk2/counting-valleys.py
+++ wk2/counting-valleys.py
@@ -1,13 +1,5 @@
 #!/bin/python3
 
-#
-# Complete the 'countingValleys' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts following parameters:
-#  1. INTEGER steps
-#  2. STRING path
-#
 
 def countingValleys(steps, path):
 
