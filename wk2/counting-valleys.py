7f3ec0d383a9983bcf9ee4a63ecca7aec3260e83
Edward <fantom-exe@tuta.io>
2022-09-02 17:28:12 -0700

    done

--- wk2/counting-valleys.py
+++ wk2/counting-valleys.py
@@ -2,9 +2,22 @@
 
 
 def countingValleys(steps, path):
+    altitude = 0
+    valleys = 0
+
+    for i in range(steps):
+        if path[i] == 'D':
+            altitude -= 1
+        elif path[i] == 'U':
+            altitude += 1
+
+        if altitude == 0:
+            if path[i] == 'U':
+                valleys += 1
+
+    return valleys
 
 
-
-steps = 0
-path = 0
+steps = 8
+path = 'UDDDUDUU'
 print(countingValleys(steps, path))
