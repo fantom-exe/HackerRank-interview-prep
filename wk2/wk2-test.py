4cbe6e6b9eff1be415ca374fffa488de698b34df
Edward <fantom-exe@tuta.io>
2022-09-03 02:12:15 -0700

    init

---
+++ wk2/wk2-test.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#
+# Complete the 'flippingMatrix' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY matrix as parameter.
+#
+
+def flippingMatrix(matrix):
+    sum = 0
+
+
+
+    return sum
+
+
+matrix = [[112, 42, 83, 119], [56, 125, 56, 49], [15, 78, 101, 43], [62, 98, 114, 108]]
+print(flippingMatrix(matrix))
