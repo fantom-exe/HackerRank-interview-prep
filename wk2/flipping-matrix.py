8e311c5622915cf7c5e60d2131b2368926ab5b2a
Edward <fantom-exe@tuta.io>
2022-09-26 20:09:27 -0700

    rename

---
+++ wk2/flipping-matrix.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+
+def flippingMatrix(matrix):
+    rows = len(matrix)
+    cols = len(matrix[0])
+    matrix_reversed = []
+    lg_sum = 0
+
+    for r in range(rows):
+        curr_sum = 0
+
+        for c in range(cols):
+            curr_sum += matrix[r][c]
+
+            # test if current sum is greater
+            if curr_sum > lg_sum:
+                lg_sum = curr_sum
+
+    return lg_sum
+
+
+matrix = [[112, 42, 83, 119],
+          [56, 125, 56, 49],
+          [15, 78, 101, 43],
+          [62, 98, 114, 108]]
+print(flippingMatrix(matrix))
